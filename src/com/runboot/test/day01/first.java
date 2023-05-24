c068c9646f9df1506f785687dfcb70ffdb12f4ed
PTE-WG11 <w1223350664@Outlook.com>
2023-05-24 23:58:17 +0800

    now

---
+++ src/com/runboot/test/day01/first.java
@@ -0,0 +1,13 @@
+package com.runboot.test.day01;
+
+public class first {
+/*
+ *  public : 访问修饰符
+ *  static : 关键字
+ *  String ： 类
+ *  args   ： 数组
+*/
+public static void main01(String[] args) {
+    System.out.println("Hello World");
+}
+}
